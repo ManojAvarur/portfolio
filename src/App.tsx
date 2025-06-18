ba96092375631d14b6324631678f4b7436381802
Manoj A M <manojavarur@gmail.com>
2025-06-18 16:58:11 -0700

    Changed from Main to Working on it after loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import { useState } from "react";
 // Components Import
 import Loading from "./components/Loading/Loading";
 import Main from "./components/Main/Main";
+import WorkingOnIt from "./components/WorkingOnIt/WorkingOnIt";
 
 export default function App() {
     const [isLoading, setIsLoading] = useState(true);
 
     function onLoadingComplete(){
+        // window.location.reload();
         setIsLoading(false);
     }
 
     return (
         <div>
             {isLoading && <Loading onComplete={onLoadingComplete} />}
-            {!isLoading && <Main />}
+            {!isLoading && <WorkingOnIt />}
         </div>
     );
 }
