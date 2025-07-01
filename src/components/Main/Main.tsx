b490da07063d9d1840325cc86fdccd87f8a462ae
Manoj A M <manojavarur@gmail.com>
2025-06-30 22:35:36 -0700

    Updated Main comp with new Menu style and colors

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,218 +1,127 @@
 "use client";
 
-import { useState } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import logo5 from "../../assets/logos/logo.png";
+import { useEffect, useRef, useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import final_logo from "../../assets/logos/final-logo.png";
 import styles from "./Main.module.css";
-
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 export default function Main() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
-        <div className="bg-dark">
-            <header className="sticky glass-effect bg-white/1 inset-x-0 top-0 z-50">
-                <nav aria-label="Global" className="lg:px-8">
-                    <div className="flex items-center justify-between lg:hidden p-1.5">
-                        <a href="#" className="">
-                            <span className="sr-only">Manoj | Portfolio</span>
-                            <img
-                                alt=""
-                                src={logo5}
-                                className="h-19 w-20"
-                            />
-                        </a>
-
-                        <button
-                            type="button"
-                            onClick={() => setMobileMenuOpen(true)}
-                            className="bg-transparent flex items-center justify-center rounded-md p-2.5"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="size-6 text-white" />
-                        </button>
-                    </div>
-                    
-                    <div className="hidden w-full lg:flex items-center justify-center gap-x-20 xl:gap-x-30">
-                        <a href="#" className="text-xl xl:text-2xl font-semibold text-nyanza hover:cursor-pointer hover-underline-animation">
-                            About
-                        </a>
-                        <a href="#" className="text-xl xl:text-2xl font-semibold text-nyanza hover:cursor-pointer hover-underline-animation">
-                            experience
-                        </a>
-                        <a href="#" className="-m-1.5 p-1.5 hover:cursor-pointer">
-                            <span className="sr-only">Manoj | Portfolio</span>
-                            <img
-                                alt=""
-                                src={logo5}
-                                className="h-24 w-25"
-                            />
-                        </a>
-                        <a href="#" className="text-xl xl:text-2xl  font-semibold text-nyanza hover:cursor-pointer hover-underline-animation">
-                            projects
-                        </a>
-                        <a href="#" className="text-xl xl:text-2xl font-semibold text-nyanza hover:cursor-pointer hover-underline-animation">
-                            contact
-                        </a>
-                    </div>
-                </nav>
-            </header>
-
-            <div className="relative isolate px-6  lg:px-8">
-                <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-                    <div
-                        style={{
-                        clipPath:
-                            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                        }}
-                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 custom-linear-gradient-bg-img sm:w-288.75"
-                    />
-                </div>
-
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-40">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            Announcing our next round of funding.{" "}
-                            <a href="#" className="font-semibold text-indigo-600">
-                                <span aria-hidden="true" className="absolute inset-0" />
-                                Read more <span aria-hidden="true">&rarr;</span>
+        <>
+            {/* <div className="bg-dark"> */}
+                <header className="glass-effect sticky inset-x-0 top-0 z-50 bg-white/1">
+                    <nav aria-label="Global" className="overflow-hidden lg:px-8">
+                        <div className="flex items-center justify-between p-1.5 lg:hidden">
+                            <a href="#" className="">
+                                <span className="sr-only">Manoj | Portfolio</span>
+                                <img alt="Logo" src={final_logo} className="h-19 w-20" />
                             </a>
+
+                            <div className="relative">
+                                <span className="absolute inset-0 flex items-center justify-center">
+                                    <span
+                                        className={`bg-nyanza absolute inline-flex h-12 w-12 rounded-full opacity-75 ${styles.ping}`}
+                                    ></span>
+                                </span>
+
+                                <button
+                                    type="button"
+                                    onClick={() => setMobileMenuOpen(true)}
+                                    className="relative z-10 flex cursor-pointer items-center justify-center rounded-md bg-transparent p-2.5"
+                                >
+                                    <span className="sr-only">Open main menu</span>
+                                    <Bars3Icon aria-hidden="true" className="text-nyanza size-7" />
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="text-center">
-                        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                            Data to enrich your online business
-                        </h1>
-                        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
-                        </p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
+
+                        <div className="hidden w-full items-center justify-center gap-x-20 lg:flex xl:gap-x-30">
                             <a
                                 href="#"
-                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="text-nyanza hover-underline-animation text-xl font-semibold hover:cursor-pointer xl:text-2xl"
                             >
-                                Get started
+                                About
                             </a>
-                            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                                Learn more <span aria-hidden="true">→</span>
+                            <a
+                                href="#"
+                                className="text-nyanza hover-underline-animation text-xl font-semibold hover:cursor-pointer xl:text-2xl"
+                            >
+                                experience
+                            </a>
+                            <a href="#" className="-m-1.5 p-1.5 hover:cursor-pointer">
+                                <span className="sr-only">Manoj | Portfolio</span>
+                                <img alt="" src={final_logo} className="h-24 w-25" />
+                            </a>
+                            <a
+                                href="#"
+                                className="text-nyanza hover-underline-animation text-xl font-semibold hover:cursor-pointer xl:text-2xl"
+                            >
+                                projects
+                            </a>
+                            <a
+                                href="#"
+                                className="text-nyanza hover-underline-animation text-xl font-semibold hover:cursor-pointer xl:text-2xl"
+                            >
+                                contact
                             </a>
                         </div>
-                    </div>
+                    </nav>
+                    <MobileMenu mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+                </header>
+            {/* </div> */}
+            <section>
+                <div aria-hidden="true" className="absolute inset-x-0 -top-4 -z-10  blur-3xl sm:-top-80 overflow-clip">
+                    <div
+                        style={{ clipPath: 'clip-path: polygon(50% 0%, 80% 10%, 66% 46%, 100% 70%, 69% 83%, 50% 100%, 31% 69%, 0% 70%, 0% 35%, 69% 82%);',}}
+                        className="relative left-[calc(90%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[var(--color-dark-purple)] via-[var(--color-nyanza)] via-[var(--color-bole)] via-[var(--color-chamoisee)] to-[var(--color-raisin-black)] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+                    />
                 </div>
+
                 
-                <div className="flex justify-center mt-12">
-                    <div className="-lg flex size-10 animate-bounce items-center justify-center rounded-full bg-white p-2 ring-1 ring-gray-900/5 dark:bg-white/5 dark:ring-white/20">
-                        <svg className="size-6 text-bole" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                        </svg>
-                    </div>
-                </div>
+            </section>
+        </>
 
-                <div aria-hidden="true" className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                    <div
-                        style={{
-                        clipPath:
-                            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                        }}
-                        className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 custom-linear-gradient-bg-img opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
-                    />
-                </div>
-            </div>
 
-            <div className="relative isolate px-6 pt-14 lg:px-8">
-                <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-                    <div
-                        style={{
-                        clipPath:
-                            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                        }}
-                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 custom-linear-gradient-bg-img opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-                    />
-                </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Announcing our next round of funding.{" "}
-                        <a href="#" className="font-semibold text-indigo-600">
-                            <span aria-hidden="true" className="absolute inset-0" />
-                            Read more <span aria-hidden="true">&rarr;</span>
-                        </a>
-                        </div>
-                    </div>
-                    <div className="text-center">
-                        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                        Data to enrich your online business
-                        </h1>
-                        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                        lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
-                        </p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a
-                            href="#"
-                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Get started
-                        </a>
-                        <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                            Learn more <span aria-hidden="true">→</span>
-                        </a>
-                        </div>
-                    </div>
-                </div>
-                {/* <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                >
-                    <div
-                        style={{
-                        clipPath:
-                            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                        }}
-                        className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
-                    />
-                </div> */}
-            </div>
-        </div>
     );
-  // return (
-  //     <>
-  //         <div aria-hidden="true" className="absolute inset-x-0 -top-4 -z-10  blur-3xl sm:-top-80">
-  //             <div
-  //                 style={{ clipPath: 'clip-path: polygon(50% 0%, 80% 10%, 66% 46%, 100% 70%, 69% 83%, 50% 100%, 31% 69%, 0% 70%, 0% 35%, 69% 82%);',}}
-  //                 className="relative left-[calc(90%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[var(--color-dark-purple)] via-[var(--color-nyanza)] via-[var(--color-bole)] via-[var(--color-chamoisee)] to-[var(--color-raisin-black)] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-  //             />
-  //         </div>
+    // return (
+    //     <>
+    //         <div aria-hidden="true" className="absolute inset-x-0 -top-4 -z-10  blur-3xl sm:-top-80">
+    //             <div
+    //                 style={{ clipPath: 'clip-path: polygon(50% 0%, 80% 10%, 66% 46%, 100% 70%, 69% 83%, 50% 100%, 31% 69%, 0% 70%, 0% 35%, 69% 82%);',}}
+    //                 className="relative left-[calc(90%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[var(--color-dark-purple)] via-[var(--color-nyanza)] via-[var(--color-bole)] via-[var(--color-chamoisee)] to-[var(--color-raisin-black)] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+    //             />
+    //         </div>
 
-  //         <header className='hidden bg-transparent p-6 text-amber-50'>
-  //             <nav className="lg:px-8" aria-label="Global">
-  //                 {/* // about
-  //                 // experience
-  //                 // logo
-  //                 // projects
-  //                 // contact  */}
+    //         <header className='hidden bg-transparent p-6 text-amber-50'>
+    //             <nav className="lg:px-8" aria-label="Global">
+    //                 {/* // about
+    //                 // experience
+    //                 // logo
+    //                 // projects
+    //                 // contact  */}
 
-  //                 <ul className="flex items-center justify-evenly">
-  //                     <li className="">
-  //                         <a href='#'>About</a>
-  //                     </li>
-  //                     <li className="">
-  //                         <a href='#'>Experience</a>
-  //                     </li>
-  //                     <li className="">
-  //                         <a href='#'>Logo</a>
-  //                     </li>
-  //                     <li className="">
-  //                         <a href='#'>Projects</a>
-  //                     </li>
-  //                     <li className="">
-  //                         <a href='#'>Contact</a>
-  //                     </li>
-  //                 </ul>
-  //             </nav>
-  //         </header>
-  //     </>
-  // )
+    //                 <ul className="flex items-center justify-evenly">
+    //                     <li className="">
+    //                         <a href='#'>About</a>
+    //                     </li>
+    //                     <li className="">
+    //                         <a href='#'>Experience</a>
+    //                     </li>
+    //                     <li className="">
+    //                         <a href='#'>Logo</a>
+    //                     </li>
+    //                     <li className="">
+    //                         <a href='#'>Projects</a>
+    //                     </li>
+    //                     <li className="">
+    //                         <a href='#'>Contact</a>
+    //                     </li>
+    //                 </ul>
+    //             </nav>
+    //         </header>
+    //     </>
+    // )
 }
