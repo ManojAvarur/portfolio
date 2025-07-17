14887119a2af33553be44f2713815b8a11fa2cc8
Manoj A M <manojavarur@gmail.com>
2025-07-17 11:27:40 -0700

    Mobile Menu view completed

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -3,6 +3,7 @@ import final_logo from "../../assets/logos/final-logo.png";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import styles from "./MobileMenu.module.css";
 import { useEffect, useRef } from "react";
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 
 
 type MobileMenuPops = {
@@ -12,9 +13,13 @@ type MobileMenuPops = {
 
 export default function MobileMenu({ mobileMenuOpen, setMobileMenuOpen }: MobileMenuPops) {
     const dialogPannelRef = useRef<HTMLDivElement>(null);
+    const headerContentRef = useRef<HTMLDivElement>(null);
+    const mainContentRef = useRef<HTMLDivElement>(null);
 
     function animationEndHandler(){
         if(dialogPannelRef.current?.classList.contains(styles.open)){
+            headerContentRef.current?.classList.add('animate-slide-in');
+            mainContentRef.current?.childNodes.forEach(el => (el as HTMLAnchorElement).classList.add('animate-slide-in'))
             return
         }
 
@@ -22,7 +27,14 @@ export default function MobileMenu({ mobileMenuOpen, setMobileMenuOpen }: Mobile
     }
 
     function onCloseHandler(){
-        dialogPannelRef.current?.classList.toggle('open');
+        headerContentRef.current?.classList.replace('animate-slide-in', 'animate-slide-out');
+        mainContentRef.current?.childNodes.forEach(el => {
+            const anchorTag = (el as HTMLAnchorElement);
+            anchorTag.style.animationDelay = "0s";
+            anchorTag.classList.replace('animate-slide-in', 'animate-slide-out');
+        });
+
+        dialogPannelRef.current?.classList.remove(styles.open);
     }
 
     return (
@@ -31,23 +43,60 @@ export default function MobileMenu({ mobileMenuOpen, setMobileMenuOpen }: Mobile
             onClose={onCloseHandler}
             className="lg:hidden"
         >
-            <div className="fixed inset-0 z-50" />
-            <div className={`bg-amber-900 ${styles.menu} ${styles.open}`} ref={dialogPannelRef} onAnimationEnd={animationEndHandler} />
-            <DialogPanel className="relative z-60">
-                <div className="flex items-center justify-between">
-                    <a href="#">
-                        <span className="sr-only">Your Company</span>
-                        <img alt="" src={final_logo} className="h-8 w-auto" />
-                    </a>
-                    <button
-                        type="button"
-                        onClick={() => setMobileMenuOpen(false)}
-                        className="-m-2.5 rounded-md p-2.5 text-nyanza cursor-pointer"
-                    >
-                        <span className="sr-only">Close menu</span>
-                        <XMarkIcon aria-hidden="true" className="size-6" />
-                    </button>
+            {/* <div className="fixed inset-0 z-50" /> */}
+            <div className={`bg-chamoisee transform-gpu ${styles.menu} ${styles.open}`} ref={dialogPannelRef} onAnimationEnd={animationEndHandler} />
+            <DialogPanel>
+                <div className="absolute top-0 left-1 z-50 w-full opacity-0" ref={headerContentRef}>
+                    <div className="flex items-center justify-between px-5">
+                        <a href="#">
+                            <span className="sr-only">Your Company</span>
+                            <img alt="Logo" src={final_logo} className="w-25 h-auto" />
+                        </a>
+                        <button
+                            type="button"
+                            onClick={onCloseHandler}
+                            className="-m-2.5 rounded-md p-2.5 text-nyanza cursor-pointer"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-7" />
+                        </button>
+                    </div>
                 </div>
+                <div className="absolute text-center top-1/2 left-1/2 z-50 -translate-1/2 [--delaySec:.1s]"  ref={mainContentRef}>
+                    <a
+                        href="#"
+                        className="text-nyanza text-4xl font-semibold block my-10 overflow-hidden opacity-0"
+                        style={{ animationDelay: "calc(var(--delaySec) + .0s)" }}
+                    >
+                        About
+                        {/* <ArrowUpRightIcon strokeWidth="3" width="30" className="ml-2 inline" /> */}
+                    </a>
+                    <a
+                        href="#"
+                        className="text-nyanza text-4xl font-semibold block my-10 overflow-hidden opacity-0"
+                        style={{ animationDelay: "calc(var(--delaySec) + .1s)" }}
+                    >
+                        experience
+                        {/* <ArrowUpRightIcon strokeWidth="3" width="30" className="ml-2 inline" /> */}
+                    </a>
+                    <a
+                        href="#"
+                        className="text-nyanza text-4xl font-semibold block my-10 overflow-hidden opacity-0"
+                        style={{ animationDelay: "calc(var(--delaySec) + .2s)" }}
+                    >
+                        projects
+                        {/* <ArrowUpRightIcon strokeWidth="3" width="30" className="ml-2 inline" /> */}
+                    </a>
+                    <a
+                        href="#"
+                        className="text-nyanza text-4xl font-semibold block my-10 overflow-hidden opacity-0"
+                        style={{ animationDelay: "calc(var(--delaySec) + .3s)" }}
+                    >
+                        contact
+                        {/* <ArrowUpRightIcon strokeWidth="3" width="30" className="ml-2 inline" /> */}
+                    </a>
+                </div>
+
             </DialogPanel>
         </Dialog>
     )
